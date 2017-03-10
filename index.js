eb5fd378c9f6e23dd7b6e317f8657014b55efb21
Hsu <edittye379@gmail.com>
2017-03-10 12:53:41 +0800

    :package: add lib dir

--- index.js
+++ index.js
@@ -3,20 +3,10 @@ console.log('NCU Real Time Weather is Running......')
 
 const request = require('request')
 const cheerio = require('cheerio')
-const bonus = require('./bonus')
-
-// enums week day and print it
-const weekday = new Array(7)
-weekday[0] = 'Sunday'
-weekday[1] = 'Monday'
-weekday[2] = 'Tuesday'
-weekday[3] = 'Wednesday'
-weekday[4] = 'Thursday'
-weekday[5] = 'Friday'
-weekday[6] = 'Saturday'
-let d = new Date()
-let time = `${d.getFullYear()}/${d.getMonth()+1}/${d.getDate()} ${d.getHours()}:${d.getMinutes()} ${weekday[d.getDay()]}`
-console.log(time + ' UTC+8:00\n')
+const time = require('./lib/time')
+const weather = require('./lib/weather')
+const bonus = require('./lib/bonus')
+time()
 
 request({
     url: 'http://www.ncu.edu.tw',
@@ -36,19 +26,11 @@ request({
             return
         }
 
-        // console.log(weatherHtml)
         // ref: http://stackoverflow.com/questions/17374893/how-to-extract-floating-numbers-from-strings-in-javascript
-        let weather = weatherHtml.match(/[+-]?\d+(\.\d+)?/g);
-        let temperature = weather[0]
-        let precip = parseFloat(weather[1]) || 0 // precipitation
-        console.log(`Temperature: ${temperature} \u2103`)
-        console.log(`     PRECIP: ${precip} mm/hr\n`)
-        if (temperature < 20) {
-            console.log('\u{1F455}  Remember to dress warmly')
-        }
-        if (precip > 0) {
-            console.log("\u2602  Don't forget to take your umbrella")
-        }
+        let weatherArray = weatherHtml.match(/[+-]?\d+(\.\d+)?/g);
+        let temperature = weatherArray[0]
+        let precip = parseFloat(weatherArray[1]) || 0 // precipitation
+        weather(temperature, precip)
         bonus()
     }
 )
