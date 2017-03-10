8374dc1cf6e9b16639bf4276e12f05564015cdca
Hsu <edittye379@gmail.com>
2017-03-10 17:07:32 +0800

    :package: refactor

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
 #!/usr/bin/env node
 console.log('NCU Real Time Weather is Running......')
 
-const request = require('request')
-const cheerio = require('cheerio')
 const time = require('./lib/time')
 const weather = require('./lib/weather')
 const bonus = require('./lib/bonus')
+
 console.log(time())
-
-request({
-    url: 'http://www.ncu.edu.tw',
-    method: 'GET'
-}, function(err, response, body) {
-        if (err || !body) {
-            console.log('Whoops! May be http://www.ncu.edu.tw is dead, error message: \n')
-            console.log(err + '\n')
-            return err
-        }
-
-        // paring data
-        let $ = cheerio.load(body)
-        let weatherHtml = $('.text01').text()
-        if (!weatherHtml) {
-            console.log('Whoops! The program is outdate, cann\'t get things from http://www.ncu.edu.tw')
-            return
-        }
-
-        // ref: http://stackoverflow.com/questions/17374893/how-to-extract-floating-numbers-from-strings-in-javascript
-        let weatherArray = weatherHtml.match(/[+-]?\d+(\.\d+)?/g);
-        let temperature = weatherArray[0]
-        let precip = parseFloat(weatherArray[1]) || 0 // precipitation
-        console.log(weather(temperature, precip))
-        console.log(bonus())
-    }
-)
+weather.ncu().then(data => {
+    console.log(weather.resolve(data))
+    console.log(bonus())
+}).catch(err => {
+    console.log(err)
+})
